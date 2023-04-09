52c2600e311eee101420cb721feeb53fbc32db0c
Divya Mehta <dm27091996@gmail.com>
2023-04-09 16:33:55 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DMSD-Project
+
+# Take a git pull from "https://github.com/dm0927/DMSD-Project.git"
+
+# Go to the folder and type the command on terminal "python -m venv env"
+
+# Once the command is executed then type "source env/bin/activate"
+
+# Once the command is executed then type "pip install -r requirements.txt"
+
+# Once the command is executed then type "python main.py"
+
+The last command will execute the code
+
--- views/index.py
+++ views/index.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 home = Blueprint('home', __name__, url_prefix='/')
 
 # TODO DO NOT EDIT
 @home.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@home.route('/authentication', methods=['GET','POST'])
+def authentication():
+    return redirect(url_for("home.dashboard"))
+
+@home.route('/register', methods=['GET','POST'])
+def register():
+    return render_template("register.html")
+
+@home.route('/dashboard', methods=['GET','POST'])
+def dashboard():
+    return render_template("home.html")
+
+@home.route('/logout', methods=['GET','POST'])
+def logout():
+    return redirect(url_for("home.index"))
