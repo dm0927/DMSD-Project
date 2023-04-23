234cc855d2f7db3303637521124939cd31e1f6ec
AnirudhBalaji <anirudhbalaji1304@gmail.com>
2023-04-22 23:20:28 -0400

    part

--- sql/017_insert_data_part.sql
+++ sql/017_insert_data_part.sql
@@ -1,12 +1,13 @@
-INSERT INTO PART (Name, Price) VALUES
-('Tire', 40),
-('Brake Pad', 30),
-('Electrical console', 50),
-('Internal Console', 40),
-('Oil filter', 60),
-('Air Filter', 40),
-('Intake Manifold', 120),
-('Piston', 60),
-('Suspension', 80),
-('Headlight', 40),
-('Tail light', 40);
+INSERT INTO PART (Name, Price) 
+VALUES
+        ('Tire', 40),
+        ('Brake Pad', 30),
+        ('Electrical console', 50),
+        ('Internal Console', 40),
+        ('Oil filter', 60),
+        ('Air Filter', 40),
+        ('Intake Manifold', 120),
+        ('Piston', 60),
+        ('Suspension', 80),
+        ('Headlight', 40),
+        ('Tail light', 40);
