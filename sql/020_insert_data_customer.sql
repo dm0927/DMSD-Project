9489dfb6b88af9c5f935c797dc6966d99e8e6579
AnirudhBalaji <anirudhbalaji1304@gmail.com>
2023-04-27 00:13:37 -0400

    Changes

---
+++ sql/020_insert_data_customer.sql
@@ -0,0 +1,9 @@
+INSERT INTO CUSTOMER(Name,Address,Password,Email)
+VALUES
+        ('Michael','123 Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','Michael@gmail.com'),
+        ('John','1411 Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','John@gmail.com'),
+        ('Smith','098 Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','Smith@gmail.com'),
+        ('Drake','12309 Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','Drake@gmail.com'),
+        ('Rat','12321 Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','Rat@gmail.com');
+        ('Pratt','213123 Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','Chris@gmail.com'),
+        ('Loki','King Street','$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','Loki@gmail.com');
---
+++ sql/021_insert_data_employee.sql
@@ -0,0 +1,9 @@
+Insert into employee(SSN,Name,Email,Password,Hire_date,Role,Location_ID )
+VALUES
+        (123345,'Viswanath',"vishwa@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Admin',Null),
+        (1233236,'Manager1',"Manager1@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Manager',1),
+        (12332312,'Manager2',"Manager2@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Manager',2),
+        (123323,'Manager3',"Manager3@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Manager',3),
+        (1233239,'Manager4',"Manager4@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Manager',4),
+        (1233237,'Manager5',"Manager5@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Manager',5),
+        (1233238,'Manager6',"Manager6@gmail.com",'$2b$12$fgepKlQ9AJ3BGp10ioAxQuYkI39lOizgMi8zgFYTzH4SKucXx99Oy','2022-01-01','Manager',6);
